ac11355a7d88242300d27162a590ce8c05338c16
Toan Tran Vu <toan.tran@dinosys.vn>
2017-12-03 17:27:50 +0700

    Removed momentjs

--- dist/lib/core/search_read.js
+++ dist/lib/core/search_read.js
@@ -4,8 +4,11 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = search_read;
-function search_read(model, domain, kwargs, callback) {
-  this.call_kw(model, 'search_read', [domain], kwargs, function (err, resp) {
+function search_read(model, domain, fields, kwargs, callback) {
+  var params = Object.assign({}, kwargs);
+  params.domain = domain;
+  params.fields = fields;
+  this.call_kw(model, 'search_read', [], params, function (err, resp) {
     if (callback) {
       var result = [];
       if (!err) {
